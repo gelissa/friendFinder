bb3686ba8facbab502ad735fbff5787a9683f447
gelissa <gelissaleveille@yahoo.com>
2019-10-09 19:33:19 -0400

    added for loop functionality and worked on modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,16 +10,50 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function (req, res) {
+        console.log(req.body)
 
-        var user = req.body;
-
-        for (i = 0; i < user.length; i++){
-            user.scores[i] = parseInt(user.scores[i])
-            for (i = 0; i < res.scores.length; i++){
-                friends.scores[i] = res.scores.length 
+        // var user = req.scores.body;
+        // OR
+        var userScore = req.body.scores; 
+        // idk which is correct
+        var scoreDifferences = [];
+        for (i = 0; i < friends.length; i++){
+            // userSum = 0;
+            // user.scores[i] = parseInt(user.scores[i]);
+            // adds each number to 0 in order to get a total
+            var currentFriendScore = friends[i].scores;
+            var match = []
+            for (j = 0; j < currentFriendScore.length; j++){
+                
+                var math = parseInt(userScore[i]) - parseInt(currentFriendScore[j]);
+                match.push(Math.abs(math));
                 
             }
+            
+            var sum = match.reduce((a, b) => a + b, 0);
+                
+            scoreDifferences.push(sum);
+
+            console.log(scoreDifferences)
+            // for (var i in user.scores) {userSum += user.scores[i]};
+            // // loops thru each friend score
+            // for (i = 0; i < res.scores.length; i++){
+            //     friendSum = 0
+            //     friends.scores[i] = parseInt(res.scores.length)
+            //     // why don't i copy the for/in loop here?
+            //     var matchSearch = friendSum += friends.scores[i];
+            //     if (matchSearch === userSum) {
+            //         friends.push(user)
+            //         console.log("you've got a match")
+            //     } 
+                
+            // }
         }
+
+        var userMatch = Math.min(...scoreDifferences);
+        console.log(userMatch);
+        var userMatchIndex = scoreDifferences.indexOf(userMatch);
+        var friend = friends[userMatchIndex];
 
         // this is where the logic goes
         // compares your scores with the scores from the friends in the data
@@ -34,9 +68,9 @@ module.exports = function(app) {
 
 
         // ====================
-        var newFriend = req.body;
-        console.log(newFriend);
-        friends.push(newFriend);
-        res.json(newFriend);
+        // var newFriend = req.body;
+        // console.log(newFriend);
+        // friends.push(newFriend);
+        res.json(friend);
     });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,18 +4,19 @@ module.exports = function(app){
 
     app.get("/", function (req, res) {
 
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../../public/home.html"));
 
     });
 
     app.get("/survey", function (req, res) {
 
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../../public/survey.html"));
 
     });
 
     app.get("*", function (req, res) {
         // maybe insert a cute 404 page if i have time
+        res.sendFile(path.join(__dirname, "../../public/home.html"));
     });
 };
 
