d93616ead1952c75b44387ade6aaa08af672c9ce
gelissa <gelissaleveille@yahoo.com>
2019-10-08 21:23:47 -0400

    randomized friend answers using Math.random

--- app/data/friends.js
+++ app/data/friends.js
@@ -38,31 +38,31 @@ var friends = [
         "name":"Ovie",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/love-island-sr5-92-0-1562134231.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
-            1,
+            2,
             4,
+            3,
             4,
             5,
-            1,
+            4,
             2,
             5,
-            4,
-            1
+            5,
+            2
           ]
       },
       {
         "name":"Jourdan",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/love-island-sr5-85-0-1562134207.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
+            3,
+            2,
+            2,
+            2,
+            2,
             1,
-            4,
-            4,
-            5,
-            1,
+            3,
             2,
             5,
-            4,
             1
           ]
       },
@@ -70,63 +70,63 @@ var friends = [
         "name":"Maura",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/love-island-mauri-1560281470.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
+            3,
+            3,
+            4,
+            1,
+            3,
+            1,
+            3,
             1,
             4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            3
           ]
       },
       {
         "name":"Sherif",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sherif-lanre-love-island-sr5-14-jpg-1558964468.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
             1,
+            3,
             4,
             4,
-            5,
-            1,
+            4,
             2,
-            5,
+            2,
+            1,
             4,
-            1
+            3
           ]
       },
       {
         "name":"Tommy",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tommy-fury-love-island-sr5-38-jpg-1558964137.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
+            3,
+            3,
             1,
-            4,
-            4,
+            3,
             5,
-            1,
+            2,
             2,
             5,
-            4,
-            1
+            2,
+            3
           ]
       },
       {
         "name":"Amber",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/amber-gill-love-island-sr5-05-jpg-1558960784.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
+            3,
+            3,
             1,
             4,
             4,
-            5,
-            1,
             2,
-            5,
+            1,
             4,
+            2,
             1
           ]
       },
@@ -134,16 +134,16 @@ var friends = [
         "name":"Curtis",
         "photo":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/curtis-pritchard-love-island-sr5-50-jpg-1558963922.jpg?crop=1xw:1xh;center,top&resize=980:*",
         "scores":[
-            5,
             1,
+            5,
             4,
             4,
             5,
-            1,
-            2,
-            5,
+            3,
             4,
-            1
+            4,
+            3,
+            3
           ]
       },
       {
