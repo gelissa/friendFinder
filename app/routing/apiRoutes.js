338e74d7330fa335a43bea24c0522fa6ec0bfdb3
gelissa <gelissaleveille@yahoo.com>
2019-10-08 22:44:41 -0400

    added jquery and onclicks

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ var friends = require("../data/friends");
 
 
 module.exports = function(app) {
+    // this will dispaly all the friends in the array
     app.get("/api/friends", function (req, res) {
         // passes the friends array as a json object onto the page
         res.json(friends);
@@ -9,6 +10,16 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function (req, res) {
+
+        var user = req.body;
+
+        for (i = 0; i < user.length; i++){
+            user.scores[i] = parseInt(user.scores[i])
+            for (i = 0; i < res.scores.length; i++){
+                friends.scores[i] = res.scores.length 
+                
+            }
+        }
 
         // this is where the logic goes
         // compares your scores with the scores from the friends in the data
@@ -19,5 +30,13 @@ module.exports = function(app) {
         // subtract each of the scores and get the distance. add the differences
         // the users with the least difference is a match.
         // push the newest friend after the math is done or else you'll match with yourself.  
+        
+
+
+        // ====================
+        var newFriend = req.body;
+        console.log(newFriend);
+        friends.push(newFriend);
+        res.json(newFriend);
     });
 };
