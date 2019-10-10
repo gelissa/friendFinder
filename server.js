3cdae69794053ae06828e3a1cd890eb22da232c9
gelissa <gelissaleveille@yahoo.com>
2019-10-09 20:48:32 -0400

    changed PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
